7dcd8ae54186cdc0eb47b763172f556f84639f93
quynv-1217 <nguyen.van.quy-b@sun-asterisk.com>
2021-09-30 23:38:42 +0700

    chore: add katex css

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -36,6 +36,7 @@
 
   @include ('comments/_list')
 @endsection
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.13.18/dist/katex.min.css" integrity="sha384-zTROYFVGOfTw7JV7KUu8udsvW2fx4lWOsCEDqhBreBwlHI4ioVRtmIvEThzJHGET" crossorigin="anonymous">
 
 <script type="module">
   $(document).ready(function() {
